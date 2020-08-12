9823df781657f0dea96ebdd446b376352d38c8a3
FengFanChen <48934331+fengfanchen@users.noreply.github.com>
2020-08-12 09:27:42 +0800

    CXF Service Demo Interceptor
    CXF Service Demo Interceptor

---
+++ CXFServiceDemoInterceptor/Service/src/main/java/cn/it1995/Main.java
@@ -0,0 +1,26 @@
+package cn.it1995;
+
+import cn.it1995.interceptor.MyInterceptor;
+import cn.it1995.service.MyService;
+import cn.it1995.service.impl.MyServiceImpl;
+import org.apache.cxf.interceptor.LoggingInInterceptor;
+import org.apache.cxf.interceptor.LoggingOutInterceptor;
+import org.apache.cxf.jaxws.JaxWsServerFactoryBean;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        MyService myService = new MyServiceImpl();
+        JaxWsServerFactoryBean factoryBean = new JaxWsServerFactoryBean();
+        factoryBean.setAddress("http://127.0.0.1/MyService");   //设置暴露地址
+        factoryBean.setServiceClass(MyService.class);   //接口
+        factoryBean.setServiceBean(myService);  //设置实现类
+
+        factoryBean.getInInterceptors().add(new LoggingInInterceptor());
+        factoryBean.getOutInterceptors().add(new LoggingOutInterceptor());
+        factoryBean.getInInterceptors().add(new MyInterceptor());
+
+        factoryBean.create();   //创建webservice接口
+    }
+}
