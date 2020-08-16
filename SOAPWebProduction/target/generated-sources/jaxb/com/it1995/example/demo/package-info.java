486d0b5a57f6b224c0347701f526e7d27e787f75
FengFanChen <48934331+fengfanchen@users.noreply.github.com>
2020-08-16 09:55:56 +0800

    SOAP Web Production
    SOAP Web Production

---
+++ SOAPWebProduction/target/generated-sources/jaxb/com/it1995/example/demo/package-info.java
@@ -0,0 +1,9 @@
+//
+// 此文件是由 JavaTM Architecture for XML Binding (JAXB) 引用实现 v2.3.2 生成的
+// 请访问 <a href="https://javaee.github.io/jaxb-v2/">https://javaee.github.io/jaxb-v2/</a> 
+// 在重新编译源模式时, 对此文件的所有修改都将丢失。
+// 生成时间: 2020.08.15 时间 07:16:18 PM CST 
+//
+
+@javax.xml.bind.annotation.XmlSchema(namespace = "http://it1995.com/example/demo", elementFormDefault = javax.xml.bind.annotation.XmlNsForm.QUALIFIED)
+package com.it1995.example.demo;
