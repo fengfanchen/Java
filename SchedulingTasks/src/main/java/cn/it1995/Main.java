efbf1e8e84a66438d995037513e049afa4ab041f
FengFanChen <48934331+fengfanchen@users.noreply.github.com>
2022-11-29 11:07:34 +0800

    SchedulingTasks
    SchedulingTasks

---
+++ SchedulingTasks/src/main/java/cn/it1995/Main.java
@@ -0,0 +1,15 @@
+package cn.it1995;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+@SpringBootApplication
+@EnableScheduling
+public class Main {
+
+    public static void main(String[] args){
+
+        SpringApplication.run(Main.class, args);
+    }
+}
---
+++ SchedulingTasks/src/main/java/cn/it1995/component/ScheduledTasks.java
@@ -0,0 +1,23 @@
+package cn.it1995.component;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+@Component
+public class ScheduledTasks {
+
+    private static final Logger log = LoggerFactory.getLogger(ScheduledTasks.class);
+
+    private static final SimpleDateFormat dateFormat = new SimpleDateFormat("HH:mm:ss");
+
+    @Scheduled(fixedDelay = 5000)
+    public void reportCurrentTime(){
+
+        log.info("The time is now {}", dateFormat.format(new Date()));
+    }
+}
